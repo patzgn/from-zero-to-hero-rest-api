93a02983a862b09f53240de300ed6369a6f6f65c
patzgn <patrycja.zgierun@gmail.com>
2025-03-02 18:33:54 +0100

    Add Scalar

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,11 +1,14 @@
 using Movies.Application;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+    
 builder.Services.AddApplication();
 
 var app = builder.Build();
@@ -14,8 +17,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
