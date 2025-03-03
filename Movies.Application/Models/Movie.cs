610e5f07cfa71ad87c1339aa77eb3b5dab28f3e6
patzgn <patrycja.zgierun@gmail.com>
2025-03-03 17:21:48 +0100

    Implementing slug-based retrieval

--- Movies.Application/Models/Movie.cs
+++ Movies.Application/Models/Movie.cs
@@ -1,12 +1,26 @@
+using System.Text.RegularExpressions;
+
 namespace Movies.Application.Models;
 
-public class Movie
+public partial class Movie
 {
     public required Guid Id { get; init; }
 
     public required string Title { get; set; }
 
+    public string Slug => GenerateSlug();
+
     public required int YearOfRelease { get; set; }
 
     public required List<string> Genres { get; set; } = [];
+
+    private string GenerateSlug()
+    {
+        var sluggedTitle = SlugRegex().Replace(Title, string.Empty)
+            .ToLower().Replace(" ", "-");
+        return $"{sluggedTitle}-{YearOfRelease}";
+    }
+
+    [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 5)]
+    private static partial Regex SlugRegex();
 }
--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -18,6 +18,12 @@ public class MovieRepository : IMovieRepository
         return Task.FromResult(movie);
     }
 
+    public Task<Movie?> GetBySlugAsync(string slug)
+    {
+        var movie = _movies.SingleOrDefault(m => m.Slug == slug);
+        return Task.FromResult(movie);
+    }
+
     public Task<IEnumerable<Movie>> GetAllAsync()
     {
         return Task.FromResult(_movies.AsEnumerable());
