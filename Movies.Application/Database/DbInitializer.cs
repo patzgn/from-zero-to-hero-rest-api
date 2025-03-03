085fca9e6a7d9ac04658983a0fd8c076ff1a83b3
patzgn <patrycja.zgierun@gmail.com>
2025-03-03 20:10:57 +0100

    Removing the old in-memory database

--- Movies.Application/Database/DbInitializer.cs
+++ Movies.Application/Database/DbInitializer.cs
@@ -15,18 +15,27 @@ public class DbInitializer
     {
         using var connection = await _dbConnectionFactory.CreateConnectionAsync();
 
-        await connection.ExecuteAsync("""
-            create table if not exists movies (
-            id UUID primary key,
-            slug TEXT not null,
-            title TEXT not null,
-            yearofrelease integer not null);
-        """);
+        await connection.ExecuteAsync(
+            """
+                create table if not exists movies (
+                id UUID primary key,
+                slug TEXT not null,
+                title TEXT not null,
+                yearofrelease integer not null);
+            """);
 
-        await connection.ExecuteAsync("""
-            create unique index concurrently if not exists movies_slug_idx
-            on movies
-            using btree(slug);                               
-        """);
+        await connection.ExecuteAsync(
+            """
+                create unique index concurrently if not exists movies_slug_idx
+                on movies
+                using btree(slug);                               
+            """);
+
+        await connection.ExecuteAsync(
+            """
+                create table if not exists genres (
+                movieid UUID references movies (id),
+                name TEXT not null);
+            """);
     }
 }
