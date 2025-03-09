fe0901fe117b39e8bc5d99a0e35e03d4d7dd1fcf
patzgn <patrycja.zgierun@gmail.com>
2025-03-09 11:45:52 +0100

    Building an SDK

---
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
+using Movies.Contracts.Requests;
+using Refit;
+
+// var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+
+var services = new ServiceCollection();
+
+services
+    .AddHttpClient()
+    .AddSingleton<AuthTokenProvider>()
+    .AddRefitClient<IMoviesApi>(s => new RefitSettings
+    {
+        AuthorizationHeaderValueGetter = async (_, _) => await s.GetRequiredService<AuthTokenProvider>().GetTokenAsync()
+    })
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:5001"));
+
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var movie = await moviesApi.GetMovieAsync("nick-the-greek-2023");
+
+var newMovie = await moviesApi.CreateMovieAsync(new CreateMovieRequest
+{
+    Title = "Spider-Man 2",
+    YearOfRelease = 2002,
+    Genres = new[] { "Action" }
+});
+
+await moviesApi.UpdateMovieAsync(newMovie.Id, new UpdateMovieRequest()
+{
+    Title = "Spider-Man 2",
+    YearOfRelease = 2002,
+    Genres = new[] { "Action", "Adventure" }
+});
+
+await moviesApi.DeleteMovieAsync(newMovie.Id);
+
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3,
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
